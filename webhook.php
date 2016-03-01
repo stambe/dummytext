a576885db558ba97225283527aef2eca30fe6037
Sagar Tambe <sagar@cuelogic.co.in>
2016-03-01 17:21:42 +0530

    Changed post to request

--- webhook.php
+++ webhook.php
@@ -2,6 +2,6 @@
 print "Hello";
 $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
 $txt = "John Doe\n";
-fwrite($myfile, json_encode($_POST));
+fwrite($myfile, json_encode($_REQUEST));
 fclose($myfile);
 ?>
